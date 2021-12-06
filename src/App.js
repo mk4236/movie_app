63df1d59dc075e50f7b91c5b55d86d5b898125c3
Minq <mk4236@naver.com>
2021-12-06 15:12:26 +0900

    Movie app made with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import Contents from './components/Contents';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'welcome',
+            subject: {title: 'WEB', sub: 'World Wide Web'},
+            welcome: {title: 'Welcome', desc: 'Hello! React!!'},
+            contents: [
+                {id:1, title:'HTML', desc: 'HTML is good'},
+                {id:2, title:'CSS', desc: 'CSS is good'},
+                {id:3, title:'PUBLISHING', desc: 'PUBLISHING is good'}
+            ]
+        }
+    }
+    render() {
+        console.log("App Render");
+
+        let _title, _desc = null;
+        let mode = this.state.mode;
+        if (mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (mode === 'read') {
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
+
+        return (
+            <div className="App">
+                <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+                <TOC data={this.state.contents}></TOC>
+                <Contents title={_title} desc={_desc}></Contents>
+            </div>
+        )
+    }
 }
 
 export default App;
